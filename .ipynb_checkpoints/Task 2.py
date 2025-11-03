a082297033ed65ed3b488ce7d33d645579b9702c
aashish1943 <aashish.1943@gmail.com>
2025-11-03 17:23:11 +0530

    TASK

---
+++ .ipynb_checkpoints/Task 2.py
@@ -0,0 +1,3 @@
+First_Name = str(input("Enter First Name:"))
+Last_Name = str(input("Enter Last Name:"))
+print("Hello",First_Name,Last_Name,"Wellcome to the python program")
--- Task 1.py
+++
@@ -1,6 +0,0 @@
-a= int(input("Enter a number1: "))
-b= int(input("Enter a number2: "))
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
