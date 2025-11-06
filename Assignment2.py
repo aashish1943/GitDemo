eaff233cb5c040c77422319f0d1b1584a1103b63
aashish1943 <aashish.1943@gmail.com>
2025-11-06 17:53:02 +0530

    Assignment2

---
+++ Assignment2.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter a number1: "))
+if num1 % 2 == 0:
+    print(f"{num1} is a Even number")
+else:
+    print(f"{num1} is a Odd number")
+
+num2 = int(input("Enter a number2: "))
+if num2 % 2 == 0:
+    print(f"{num2} is a Even number")
+else:
+    print(f"{num2} is a Odd number")
